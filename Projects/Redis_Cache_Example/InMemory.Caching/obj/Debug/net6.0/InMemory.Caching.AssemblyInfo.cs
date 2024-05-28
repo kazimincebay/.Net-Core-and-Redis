31283e87608d92009e15dda415236a2242d1a651
Kazım İncebay <kazimincebay@gmail.com>
2024-05-28 13:58:14 +0300

    Redis Pub.Sub Example

--- Projects/Redis_Cache_Example/InMemory.Caching/obj/Debug/net6.0/InMemory.Caching.AssemblyInfo.cs
+++ Projects/Redis_Cache_Example/InMemory.Caching/obj/Debug/net6.0/InMemory.Caching.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InMemory.Caching")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6db4acd8eacb14834fe790726e211e382c033d4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7629950f2281af1eea3f812c3fd4eb2df5e72bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("InMemory.Caching")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InMemory.Caching")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Projects/Redis_Cache_Example/Redis.Pub.Sub.Subscriber.Example/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+ISubscriber subscriber = connection.GetSubscriber();
+await subscriber.SubscribeAsync("mychannel", (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+
+//Console.Read();
+
+/* pattern matching subscription - abonelerin belirli kalıplarda mesajları alabilmesi
+
+await subscriber.SubscribeAsync("mychannel*", (channel, message) =>
+{
+    Console.WriteLine(message);
+});
+
+Console.Read();
+
+Burada mychannel. ile başlayan mesajları kabul edecektir mychannel.google mychannel.facebook gibi kanalları dinler.
+bu kanallara subscribe olmak için psubscribe mychannel.* şeklinde subscribe olunur.
+*/
